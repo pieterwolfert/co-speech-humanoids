5bd26fe093d8a44795661cf2f5bcd46d07bdfc06
Pieter Wolfert <pieter.wolfert@ugent.be>
2019-03-27 13:18:38 +0100

    updated main

--- seq2seq.py
+++ seq2seq.py
@@ -197,33 +197,3 @@ class Seq2Pose():
             'decoder_optimizer_state_dict': dec_optimizer.state_dict(),
             'loss': loss,
             }, PATH)
-
-def main():
-    batch_size = 64
-    dataset_size = 100
-    hidden_size = 200
-    bidirectional = True
-    embedding_size = 300
-    epochs = 10
-    learning_rate = 0.0001
-    clip = 5.0
-    alpha = 0.1
-    beta = 1
-    #n and m are parameters from the paper
-    n = 10
-    m = 20
-
-    #dataloader loads our preprocessed pickle file, and the pretrained embedding.
-    dl = DataLoader("./data/preprocessed_1295videos.pickle",\
-        "./data/glove.6B.300d.txt", dataset_size, 10)
-    x_train, y_train = dl.getTrainingData()
-
-    #we make sure the embedding is a torch tensor
-    wm = dl.getEmbeddingMatrix(300)
-    wm = torch.from_numpy(wm).float().to(device)
-    sq = Seq2Pose(wm, 24, batch_size, hidden_size, True, embedding_size, n , m,\
-        learning_rate, clip, alpha, beta)
-    sq.train(epochs, x_train, y_train)
-
-if __name__=="__main__":
-    main()
