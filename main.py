e37522b71691ec44de042a0560e6cbe5868d161a
eddie <ehwa009@aucklanduni.ac.nz>
2019-10-28 14:59:36 +0900

    commit_by_Eddie

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from seq2seq import Seq2Pose
 from dataloader import DataLoader
 import torch
-
-__author__ = "Pieter Wolfert"
+import os
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+torch.backends.cudnn.benchmark = True
+torch.multiprocessing.set_sharing_strategy('file_system')
 
 def main():
-    batch_size = 128
+    batch_size = 64
     dataset_size = 10000
+
+    # batch_size = 3
+    # dataset_size = 3
+
     hidden_size = 200
     bidirectional = True
     embedding_size = 300
@@ -17,21 +22,23 @@ def main():
     clip = 5.0
     alpha = 0.1
     beta = 1
-    #n and m are parameters from the paper
+    
+    # n and m are parameters from the paper
     n = 10
     m = 20
-
-    #dataloader loads our preprocessed pickle file, and the pretrained embedding.
-    dl = DataLoader("./data/preprocessed_1295videos.pickle",\
+    
+    dl = DataLoader("./data/ted_gesture_dataset_train.pickle",\
         "./data/glove.6B.300d.txt", dataset_size, 10)
     x_train, y_train = dl.getTrainingData()
+    # print("paris of sequence of words and poses: {}".format(len(x_train)))
 
-    #we make sure the embedding is a torch tensor
+    # we make sure the embedding is a torch tensor
     wm = dl.getEmbeddingMatrix(300)
     wm = torch.from_numpy(wm).float().to(device)
-    sq = Seq2Pose(wm, 24, batch_size, hidden_size, True, embedding_size, n , m,\
-        learning_rate, clip, alpha, beta, pre_trained_file = "./models/seq2seq_10_0.13292566299438477.tar")
-    sq.train(epochs, x_train, y_train)
+    sq = Seq2Pose(wm, 24, batch_size, hidden_size, True, embedding_size, n , m, 
+                    learning_rate, clip, alpha, beta)
+                    # pre_trained_file="./models/seq2seq_2_4619870.238758475.tar")
+    sq.trainIter(x_train, y_train, epochs, num_workers=6)
 
 if __name__=="__main__":
     main()
